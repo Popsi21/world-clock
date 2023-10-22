e6d73bf2d463f8705b128af95241ffae5cb58e19
Popsi21 <paulinetravers7@gmail.com>
2023-10-22 11:56:52 +0200

    added moment js tz and current time

---
+++ src/script.js
@@ -0,0 +1,22 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#newYork");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = moment().format(`MMMM Do YYYY`);
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = moment().format(`MMMM Do YYYY`);
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
